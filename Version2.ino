d917c3b4971745cdc111dad8e69cc7db74581e8a
Snowman4756 <120766282+Snowman4756@users.noreply.github.com>
2026-05-25 11:30:21 +0500

    Refactor motor control logic for clarity

--- Version2.ino
+++ Version2.ino
@@ -2,8 +2,8 @@
 #define RB 7   // Right motor backward
 #define LF 8   // Left motor forward
 #define LB 9   // Left motor backward
-#define RA 5  // Right analog
-#define LA 10   // Left analog
+#define RA 5   // Right analog
+#define LA 10  // Left analog
 
 // 7 IR Sensors //
 #define IR1 A0 // Left extreme
@@ -51,78 +51,78 @@ void loop() {
   // If CENTER
   if ((RE == 1) && (RH == 1) && (R == 1) && (C == 0) && (L == 1) && (LH == 1) && (LE == 1)) {
     // Go forward
-    analogWrite(RA, speed);   // Speed on RIGHT
-    analogWrite(LA, speed);   // Speed on LEFT
-    digitalWrite(RB, LOW);    // Right motor Backwards
-    digitalWrite(RF, HIGH);   // Right motor Forward
-    digitalWrite(LB, LOW);    // Left  motor Backwards
-    digitalWrite(LF, HIGH);   // Left  motor Forward
-  }
+    analogWrite(RA, speed);       // Speed on RIGHT
+    analogWrite(LA, speed);       // Speed on LEFT
+    digitalWrite(RB, LOW);        // Right motor Backwards
+    digitalWrite(RF, HIGH);       // Right motor Forward
+    digitalWrite(LB, LOW);        // Left  motor Backwards
+    digitalWrite(LF, HIGH);       // Left  motor Forward
+  } 
 
   // If RIGHT
   if ((RE == 1) && (RH == 1) && (R == 0) && (C == 1) && (L == 1) && (LH == 1) && (LE == 1)) {
     // Go RIGHT
     analogWrite(RA, speed - 100);  // Speed on RIGHT
-    analogWrite(LA, speed);   // Speed on LEFT
-    digitalWrite(RB, LOW);    // Right motor Backwards
-    digitalWrite(RF, HIGH);   // Right motor Forward
-    digitalWrite(LB, LOW);    // Left  motor Backwards
-    digitalWrite(LF, HIGH);   // Left  motor Forward
+    analogWrite(LA, speed);        // Speed on LEFT
+    digitalWrite(RB, LOW);         // Right motor Backwards
+    digitalWrite(RF, HIGH);        // Right motor Forward
+    digitalWrite(LB, LOW);         // Left  motor Backwards
+    digitalWrite(LF, HIGH);        // Left  motor Forward
   }
   
   // If LEFT
   if ((RE == 1) && (RH == 1) && (R == 1) && (C == 1) && (L == 0) && (LH == 1) && (LE == 1)) {
     // Go forward
-    analogWrite(RA, speed);   // Speed on RIGHT
-    analogWrite(LA, speed - 100);  // Speed on LEFT
-    digitalWrite(RB, LOW);    // Right motor Backwards
-    digitalWrite(RF, HIGH);   // Right motor Forward
-    digitalWrite(LB, LOW);    // Left  motor Backwards
-    digitalWrite(LF, HIGH);   // Left  motor Forward
+    analogWrite(RA, speed);       // Speed on RIGHT
+    analogWrite(LA, speed - 100); // Speed on LEFT
+    digitalWrite(RB, LOW);        // Right motor Backwards
+    digitalWrite(RF, HIGH);       // Right motor Forward
+    digitalWrite(LB, LOW);        // Left  motor Backwards
+    digitalWrite(LF, HIGH);       // Left  motor Forward
   }
   
   // If HARD RIGHT
   if ((RE == 1) && (RH == 0) && (R == 1) && (C == 1) && (L == 1) && (LH == 1) && (LE == 1)) {
     // Go HARD RIGHT
-    analogWrite(RA, speed);   // Speed on RIGHT
-    analogWrite(LA, speed);   // Speed on LEFT
-    digitalWrite(RB, LOW);    // Right motor Backwards
-    digitalWrite(RF, LOW);    // Right motor Forward
-    digitalWrite(LB, LOW);    // Left  motor Backwards
-    digitalWrite(LF, HIGH);   // Left  motor Forward
+    analogWrite(RA, speed);       // Speed on RIGHT
+    analogWrite(LA, speed);       // Speed on LEFT
+    digitalWrite(RB, LOW);        // Right motor Backwards
+    digitalWrite(RF, LOW);        // Right motor Forward
+    digitalWrite(LB, LOW);        // Left  motor Backwards
+    digitalWrite(LF, HIGH);       // Left  motor Forward
   }
   
   // If HARD LEFT
   if ((RE == 1) && (RH == 1) && (R == 1) && (C == 1) && (L == 1) && (LH == 0) && (LE == 1)) {
     // Go HARD LEFT
-    analogWrite(RA, speed);   // Speed on RIGHT
-    analogWrite(LA, speed);   // Speed on LEFT
-    digitalWrite(RB, LOW);    // Right motor Backwards
-    digitalWrite(RF, HIGH);   // Right motor Forward
-    digitalWrite(LB, LOW);    // Left  motor Backwards
-    digitalWrite(LF, LOW);    // Left  motor Forward
+    analogWrite(RA, speed);       // Speed on RIGHT
+    analogWrite(LA, speed);       // Speed on LEFT
+    digitalWrite(RB, LOW);        // Right motor Backwards
+    digitalWrite(RF, HIGH);       // Right motor Forward
+    digitalWrite(LB, LOW);        // Left  motor Backwards
+    digitalWrite(LF, LOW);        // Left  motor Forward
   }
   
   // If EXTREME RIGHT
   if ((RE == 0) && (RH == 1) && (R ==  1) && (C == 1) && (L == 1) && (LH == 1) && (LE == 1)) {
     // Go EXTREME RIGHT
-    analogWrite(RA, speed - 150);  // Speed on RIGHT
-    analogWrite(LA, speed - 80);   // Speed on LEFT
-    digitalWrite(RB, HIGH);   // Right motor Backwards
-    digitalWrite(RF, LOW);    // Right motor Forward
-    digitalWrite(LB, LOW);    // Left  motor Backwards
-    digitalWrite(LF, HIGH);   // Left  motor Forward
+    analogWrite(RA, speed - 150); // Speed on RIGHT
+    analogWrite(LA, speed - 80);  // Speed on LEFT
+    digitalWrite(RB, HIGH);       // Right motor Backwards
+    digitalWrite(RF, LOW);        // Right motor Forward
+    digitalWrite(LB, LOW);        // Left  motor Backwards
+    digitalWrite(LF, HIGH);       // Left  motor Forward
   }
   
   // If EXTREME LEFT
   if ((RE == 1) && (RH == 1) && (R == 1) && (C == 1) && (L == 1) && (LH == 1) && (LE == 0)) {
     // Go EXTREME LEFT
-    analogWrite(RA, speed - 150);   // Speed on RIGHT
+    analogWrite(RA, speed - 150); // Speed on RIGHT
     analogWrite(LA, speed - 80);  // Speed on LEFT
-    digitalWrite(RB, LOW);    // Right motor Backwards
-    digitalWrite(RF, HIGH);   // Right motor Forward
-    digitalWrite(LB, HIGH);   // Left  motor Backwards
-    digitalWrite(LF, LOW);    // Left  motor Forward
+    digitalWrite(RB, LOW);        // Right motor Backwards
+    digitalWrite(RF, HIGH);       // Right motor Forward
+    digitalWrite(LB, HIGH);       // Left  motor Backwards
+    digitalWrite(LF, LOW);        // Left  motor Forward
   }
 
   // If LOFT
